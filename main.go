d58e7a860f9bbe6f3900e695308048e68afaf42b
Shidraw <proust.micka@gmail.com>
2020-02-11 15:18:43 +0100

    Update main.go : Block & Transaction details

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/big"
 
 	_ "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -11,7 +12,7 @@ import (
 )
 
 func main() {
-	client := ConnectClient("rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6")
+	client := ConnectClient("https://rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6")
 
 	// Get Block Number
 	header, err := client.HeaderByNumber(context.Background(), nil)
@@ -19,19 +20,41 @@ func main() {
 		log.Fatal(err)
 	}
 
-	blockNumber := header.Number
+	blockNumber := header.Number.String()
+	fmt.Println(blockNumber)
 
-	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	blockTest := big.NewInt(5952590)
+	fmt.Println(blockTest)
+
+	block, err := client.BlockByNumber(context.Background(), blockTest)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Block time: " + string(block.Time()))
+	fmt.Println(block.Hash().Hex())
+
+	for _, tx := range block.Transactions() {
+		fmt.Printf("TX Hash: %s\n", tx.Hash().Hex())
+		fmt.Printf("TX Value: %s\n", tx.Value().String())
+		fmt.Printf("TX Gas: %d\n", tx.Gas())
+		fmt.Printf("TX Gas Price: %d\n", tx.GasPrice().Uint64())
+		fmt.Printf("TX Nonce: %d\n", tx.Nonce())
+		//fmt.Printf("TX Data: %v\n", tx.Data())
+		fmt.Printf("TX To: %s\n", tx.To().Hex())
+
+		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Receipt Status: %d\n", receipt.Status)
+		fmt.Println("---")
+	}
 }
 
 // Connexion à un noeud geth Rinkeby via Infura
 func ConnectClient(url string) *Client {
-	var client, err = ethclient.Dial(url) //"rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6"
+	client, err := ethclient.Dial(url) //"rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6"
 	if err != nil {
 		fmt.Println("Error while connecting to infura")
 		log.Fatal(err)
