63620bc6320467bd9e6d2ca7d49397c6c681bb92
Shidraw <proust.micka@gmail.com>
2020-02-11 16:15:42 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	_ "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	. "github.com/ethereum/go-ethereum/ethclient"
@@ -50,6 +51,32 @@ func main() {
 		fmt.Printf("Receipt Status: %d\n", receipt.Status)
 		fmt.Println("---")
 	}
+
+	// Grab block by hash then iterate over transactions by index
+	blockHash := common.HexToHash("0x01e6b1caed7765220448df0979018f5613728ff1273f5de2f137393f4d583e5e")
+	count, err := client.TransactionCount(context.Background(), blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx := uint(0); idx < count; idx++ {
+		tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("TX Hash: %s\n", tx.Hash().Hex())
+	}
+
+	// Grab a transaction by it's individual hash
+	txHash := common.HexToHash("0xa9a42eefa76655e5298996813138e6c33fac6f89506ae233c2f0b7a4e699ed68")
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("TX Hash: %s\n", tx.Hash().Hex())
+	fmt.Printf("Pending?: %v\n", isPending)
 }
 
 // Connexion à un noeud geth Rinkeby via Infura
