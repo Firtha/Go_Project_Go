20113acfde2444c53dc8b2f2af37cebbb3ea5749
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-02-11 12:45:19 +0100

    Add go code (first sample)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Firtha/Go_Project_Go
+
+go 1.13
+
+require github.com/ethereum/go-ethereum v1.9.10
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+
+func main() {
+	ConnectClient("rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6")
+
+	// Get Block Number
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber = header.Number
+
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Block time: " + block.Time().String())
+}
+
+
+// Connexion à un noeud geth Rinkeby via Infura
+func ConnectClient(string url) *Client{
+	client, err := ethclient.Dial(url)//"rinkeby.infura.io/v3/8e2834b158fa48b0a5fb9ca0f72ce6e6"
+	if err != nil {
+		fmt.Println("Error while connecting to infura")
+		log.Fatal(err)
+	}
+}
